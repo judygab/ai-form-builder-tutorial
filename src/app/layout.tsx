eef05886882d0f91e0be19bd767954e4120d74f8
judygab <ivditigabeskiria@gmail.com>
2024-02-07 00:08:03 -0800

    update analytics script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 
+import PlausibleProvider from 'next-plausible'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -16,6 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <PlausibleProvider domain={process.env.PLAUSIBLE_DOMAIN || ""} />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
